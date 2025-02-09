f7ef2d2d639e4df1db774f0725c09d6f43249da8
harshitadatra <harshitkiran22@gmail.com>
2025-02-09 12:50:04 +0530

    Chore: Remove commented-out code

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 
 require("dotenv").config();
 const express = require("express");
-// const mongoose = require("mongoose");
 const cacheRoutes = require("./routes/cacheRoutes")
 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const connectDB = require("./db");
-
-// MongoDB Connection
-// mongoose.connect(process.env.MONGO_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// }).then(() => console.log("Connected to MongoDB"))
-// .catch((err) => console.error("MongoDB connection error:", err));// Connect to MongoDB
 connectDB();
-
 
 // Middleware
 app.use(express.json());
